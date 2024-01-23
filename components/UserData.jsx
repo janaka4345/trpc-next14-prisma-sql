1704d3f8a07c1b5a9cf42a9e54ef016c37a5eb51
banuka <banukaramanayaka76@gmail.com>
2024-01-23 22:50:36 +0530

    other:credential provider adedd

--- components/UserData.jsx
+++ components/UserData.jsx
@@ -4,7 +4,7 @@ export default function UserData() {
   const data = trpc.user.users.useQuery();
   return (
     <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>{JSON.stringify(data.data, null, 2)}</pre>
       <br />
       <br />
       <br />
